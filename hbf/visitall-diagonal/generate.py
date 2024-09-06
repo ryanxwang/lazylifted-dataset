6b3ceb3fb735071f619bfd6e20535e87fdae3d4f
Ryan Wang <ryanxiaowang2001@gmail.com>
2024-09-06 19:17:16 +1000

    Generate visitall training plans

--- hbf/visitall-diagonal/generate.py
+++ hbf/visitall-diagonal/generate.py
@@ -10,6 +10,8 @@ CONFIG = {
         "chebyshev_distance": [2.0 + 0.2 * i for i in range(30)],
         "num_goals": [1, 2, 3] * 10,
         "starting_seed": 42,
+        "solve": True,
+        "plan_dir": "5dim/training_plans",
     },
 }
 REQUIRED_DIMS = [5]
@@ -48,6 +50,14 @@ def main():
         seed = CONFIG[path]["starting_seed"]
         sides = CONFIG[path]["sides"]
         dim = CONFIG[path]["d"]
+        solve = CONFIG[path]["solve"]
+        if solve:
+            plan_dir = CONFIG[path]["plan_dir"]
+            if os.path.exists(plan_dir):
+                # remove the directory and its contents
+                os.system(f"rm -r {plan_dir}")
+            os.mkdir(plan_dir)
+
         for i in range(num_instances):
             goal_count = CONFIG[path]["num_goals"][i]
             chebyshev_distance = CONFIG[path]["chebyshev_distance"][i]
@@ -55,6 +65,10 @@ def main():
             command = f"python3 {THIS_DIR}/visitall_diagonal.py -s {sides} -d {dim} --seed {seed} -id {i} --out {full_path} --num-goals {goal_count} -dis {chebyshev_distance}"
             os.system(command)
             seed += 1
+
+            if solve:
+                command = f"python3 {THIS_DIR}/solver.py {full_path}/p{i:02}.pddl {plan_dir}/p{i:02}.plan"
+                os.system(command)
 
 
 if __name__ == "__main__":
